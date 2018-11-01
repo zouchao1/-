db48697e7d775c8b6d0b29369ebe2dcf3e195eaa
zouchao1 <987533975@qq.com>
2018-11-01 08:51:43 +0800

    token

--- front_end/src/javascripts/app.js
+++ front_end/src/javascripts/app.js
@@ -1,9 +1,19 @@
 import '../stylesheets/app.scss'
 import router from './router'
 
+import {
+    userSigninAuth
+} from './util/auth'
 const body_template = require('./views/body.html')
 
 
 // 渲染整体内容结构
 $('#wrapper').html(body_template)
-router.init();
+
+let flag = userSigninAuth();
+if (flag == true) {
+    router.init();
+    $('#wrapper').removeClass('hidden')
+} else {
+    window.location.href = "/admin.html"
+}
---
+++ front_end/src/javascripts/utils/auth.js
@@ -0,0 +1,8 @@
+const userSigninAuth = () => {
+
+}
+
+
+export {
+    userSigninAuth
+}
